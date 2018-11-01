718cd665c5e5156af09f0d6f8cc4101f6ddc7baf
aasheesh <aasheeshbareja@gmail.com>
2018-11-01 14:38:03 +0530

    new changes

--- util/mongo.js
+++ util/mongo.js
@@ -2,7 +2,7 @@
 //var mongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var userModel = require("../models/user");
-const url = 'mongodb://localhost:27017/Ostinohealth';
+const url = 'mongodb://localhost:27017/Ostinohealth_ecom';
 mongoose.connect(url);
 module.exports = {
 
